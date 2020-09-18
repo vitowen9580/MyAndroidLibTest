844096f1eb5ec9a6614e983d6a45e79941aecfe8
vitowen9580 <vito9580work@gmail.com>
2020-09-18 16:20:54 +0800

    first commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/mylibrary.java
@@ -0,0 +1,12 @@
+package com.example.mylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class mylibrary {
+
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+    }
+}
